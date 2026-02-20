55727d0d31ec03e9a5c579600cd193bc19256ce5
Manikanta Cheruku <cherukumanikanta77@gmail.com>
2026-02-20 23:48:06 +0530

    Corrected app.py

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ import numpy as np
 import joblib
 import os
 
-st.set_option('deprecation.showfileUploaderEncoding', False)
+#st.set_option('deprecation.showfileUploaderEncoding', False)
 
 # -----------------------------------------------
 # CONFIGURATION
